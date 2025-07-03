1eb39301036cac3c92342ba043472acfc4f82969
Hiroak-ueno-8408 <hiroaki2568@icloud.com>
2025-07-03 12:19:14 +0900

    Won/lost message added

---
+++ dice.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+print("Rolling the dice...")
+time.sleep(1)
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1 + die2
+
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value: {total}")
+
+if total > 7:
+    print("You won!")
+else:
+    print("You lost.")
