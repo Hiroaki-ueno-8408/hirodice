2377de160945fdd7ad94f17ad45b6890757e1dad
Hiroak-ueno-8408 <hiroaki2568@icloud.com>
2025-07-03 12:17:14 +0900

    Greeting added

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+name = input("What is your name? \n> ")
+print(f"Hello, {name}!")
+
+print("Rolling the dice...")
+time.sleep(1)               # 演出用ポーズ
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1 + die2
+
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value: {total}")
